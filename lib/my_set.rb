1f99486caa850901ed79b9e900b2c9a0cf7c9431
Aysan Isayo <aisayo@icloud.com>
2021-12-02 21:54:02 -0800

    commit tree 93c2679a9ffe4f14f137e01cfe3149ae159c8551

--- lib/my_set.rb
+++ lib/my_set.rb
@@ -1,1 +1,35 @@
-# your code here
+class MySet
+    attr_reader :hash
+    def initialize(enumerable = [])
+        @hash = {}
+        enumerable.each do |value|
+            @hash[value] = true
+        end
+    end
+
+    def include?(key)
+        hash.has_key?(key)
+    end 
+
+    def add(value)
+        hash[value] = true
+        self
+    end
+
+    def delete(value)
+        hash.delete(value)
+        self
+    end
+
+    def size
+        hash.size
+    end
+
+    def self.[](*args)
+        new(args)
+    end
+
+    def clear 
+        hash = {}
+    end
+end
